92baa1f629a78e4dc7a68db4e5e46684c925ef8b
ericbm2 <ericbmorgenstern@gmail.com>
2015-05-06 11:44:06 -0400

    more context menu stuff

--- static/thingifoundonline.js
+++ static/thingifoundonline.js
@@ -1,4 +1,6 @@
 var map;
+var latitude;
+var longitude;
 function initialize() {
     var latlng = new google.maps.LatLng(40.718,-74.0142);
     var myOptions = {
@@ -43,17 +45,76 @@ function setMenuXY(caurrentLatLng){
 };
 function showContextMenu(caurrentLatLng  ) {
     var projection;
-    var contextmenuDir;
+    var contextmenuDir1;
+    var contextmenuDir2;
+    var contextmenuDir3;
+    var contextmenuDir4;
     projection = map.getProjection() ;
     $('.contextmenu').remove();
-    contextmenuDir = document.createElement("div");
-    contextmenuDir.className  = 'contextmenu';
-    contextmenuDir.innerHTML = "<a id='menu1'><div class=context>Place Pothole Here<\/div><\/a><a id='menu2'><div class=context>Get Directions To Here<\/div><\/a><a id='menu3'><div class=context>Get Directions From Here<\/div><\/a>";
-    $(map.getDiv()).append(contextmenuDir);
     
+    contextmenuDir1 = document.createElement("div");
+    contextmenuDir1.className  = 'contextmenu';
+    contextmenuDir1.innerHTML = "<a id='menu1'><div class=context>Place Pothole Here<\/div><\/a>";
+    $(map.getDiv()).append(contextmenuDir1);    
     setMenuXY(caurrentLatLng);
+    contextmenuDir1.style.visibility = "visible";
+    var e1 = document.getElementById("menu1");
+    google.maps.event.addDomListener(e1,"click",function(event){
+	console.log("menu1");
+	contextmenuDir1.style.visibility="hidden";
+	contextmenuDir2.style.visibility="hidden";
+	contextmenuDir3.style.visibility="hidden";
+	contextmenuDir4.style.visibility="hidden";
+    });
     
-    contextmenuDir.style.visibility = "visible";
+    contextmenuDir2 = document.createElement("div");
+    contextmenuDir2.className  = 'contextmenu';
+    contextmenuDir2.innerHTML = "<a id='menu2'><div class=context>Get Directions To Here<\/div><\/a>";
+    $(map.getDiv()).append(contextmenuDir2);
+    setMenuXY(caurrentLatLng);
+    contextmenuDir2.style.visibility = "visible";
+    var e2 = document.getElementById("menu2");
+    google.maps.event.addDomListener(e2,"click",function(event){
+	console.log("menu2");
+	latitude = event.latLng.lat();
+	longitude = event.latLng.lng();
+	console.log(latitude);
+	console.log(longitude);
+	contextmenuDir1.style.visibility="hidden";
+	contextmenuDir2.style.visibility="hidden";
+	contextmenuDir3.style.visibility="hidden";
+	contextmenuDir4.style.visibility="hidden";
+    });
+    
+    contextmenuDir3 = document.createElement("div");
+    contextmenuDir3.className  = 'contextmenu';
+    contextmenuDir3.innerHTML = "<a id='menu3'><div class=context>Get Directions From Here<\/div><\/a>";
+    $(map.getDiv()).append(contextmenuDir3);
+    setMenuXY(caurrentLatLng);
+    contextmenuDir3.style.visibility = "visible";
+    var e3 = document.getElementById("menu3");
+    google.maps.event.addDomListener(e3,"click",function(event){
+	console.log("menu3");
+	contextmenuDir1.style.visibility="hidden";
+	contextmenuDir2.style.visibility="hidden";
+	contextmenuDir3.style.visibility="hidden";
+	contextmenuDir4.style.visibility="hidden";
+    });
+
+    contextmenuDir4 = document.createElement("div");
+    contextmenuDir4.className  = 'contextmenu';
+    contextmenuDir4.innerHTML = "<a id='menu4'><div class=context>Cancel<\/div><\/a>";
+    $(map.getDiv()).append(contextmenuDir4);    
+    setMenuXY(caurrentLatLng);
+    contextmenuDir4.style.visibility = "visible";
+    var e4 = document.getElementById("menu4");
+    google.maps.event.addDomListener(e4,"click",function(event){
+	console.log("menu4");
+	contextmenuDir1.style.visibility="hidden";
+	contextmenuDir2.style.visibility="hidden";
+	contextmenuDir3.style.visibility="hidden";
+	contextmenuDir4.style.visibility="hidden";
+    });
 }
 $(document).ready(function(){
     initialize();
