b58224a98c82444e3d09e37fbea133f7a919566c
ericbm2 <ericbmorgenstern@gmail.com>
2015-05-08 11:43:45 -0400

    trying to get search box

--- static/thingifoundonline.js
+++ static/thingifoundonline.js
@@ -3,9 +3,10 @@ var latitude;
 var longitude;
 var toHere = null;
 var fromHere = null;
-var toHereMarker;
-var fromHereMarker;
-var potholeHere;
+var toHereMarker = null;
+var fromHereMarker = null;
+var potholeHere = null;
+var potholeMarker = null;
 function initialize() {
     var latlng = new google.maps.LatLng(40.718,-74.0142);
     var myOptions = {
@@ -21,7 +22,54 @@ function initialize() {
 	console.log(longitude);
 	showContextMenu(event.latLng);
     });
-    
+    var input = /** @type {HTMLInputElement} */(
+	document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(
+	/** @type {HTMLInputElement} */(input));
+    // [START region_getplaces]
+    // Listen for the event fired when the user selects an item from the
+    // pick list. Retrieve the matching places for that item.
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+	var places = searchBox.getPlaces();
+	if (places.length == 0) {
+	    return;
+	}
+	for (var i = 0, marker; marker = markers[i]; i++) {
+	    marker.setMap(null);
+	}
+	console.log(places[0].formatted_address);
+	calcRoute(places[0].formatted_address);
+	// For each place, get the icon, place name, and location.
+	markers = [];
+	var bounds = new google.maps.LatLngBounds();
+	for (var i = 0, place; place = places[i]; i++) {
+	    var image = {
+		url: place.icon,
+		size: new google.maps.Size(71, 71),
+		origin: new google.maps.Point(0, 0),
+		anchor: new google.maps.Point(17, 34),
+		scaledSize: new google.maps.Size(25, 25)
+	    };
+	    // Create a marker for each place.
+	    var marker = new google.maps.Marker({
+		map: map,
+		icon: image,
+		title: place.name,
+		position: place.geometry.location
+	    });
+	    markers.push(marker);
+	    bounds.extend(place.geometry.location);
+	}
+	map.fitBounds(bounds);
+    });
+    // [END region_getplaces]
+    // Bias the SearchBox results towards places that are within the bounds of the
+    // current map's viewport.
+    google.maps.event.addListener(map, 'bounds_changed', function() {
+	var bounds = map.getBounds();
+	searchBox.setBounds(bounds);
+    });
 }
 function getCanvasXY(caurrentLatLng){
     var scale = Math.pow(2, map.getZoom());
@@ -79,6 +127,12 @@ function showContextMenu(caurrentLatLng  ) {
 	contextmenuDir3.style.visibility="hidden";
 	contextmenuDir4.style.visibility="hidden";
 	contextmenuDir5.style.visibility="hidden";
+	potholeHereMarker = new google.maps.Marker({
+	    position: potholeHere,
+	    map: map,
+	    title: 'Pothole'
+	});
+	//Mark's pothole function
     });
     
     contextmenuDir2 = document.createElement("div");
@@ -96,6 +150,15 @@ function showContextMenu(caurrentLatLng  ) {
 	contextmenuDir3.style.visibility="hidden";
 	contextmenuDir4.style.visibility="hidden";
 	contextmenuDir5.style.visibility="hidden";
+	toHereMarker = new google.maps.Marker({
+	    position: toHere,
+	    map: map,
+	    title: 'Destination'
+	});
+	if(fromHere != null){
+	    console.log("getting directions");
+	    //calcRoute(fromHere,toHere);
+	};
     });
     
     contextmenuDir3 = document.createElement("div");
@@ -113,6 +176,15 @@ function showContextMenu(caurrentLatLng  ) {
 	contextmenuDir3.style.visibility="hidden";
 	contextmenuDir4.style.visibility="hidden";
 	contextmenuDir5.style.visibility="hidden";
+	fromHereMarker = new google.maps.Marker({
+	    position: fromHere,
+	    map: map,
+	    title: 'Starting Point'
+	});
+	if(toHere != null){
+	    console.log("getting directions");
+	    //calcRoute(fromHere,toHere);
+	};
     });
 
     contextmenuDir4 = document.createElement("div");
@@ -126,6 +198,10 @@ function showContextMenu(caurrentLatLng  ) {
 	console.log("menu4");
 	toHere = null;
 	fromHere = null;
+	fromHereMarker.setMap(null);
+	toHereMarker.setMap(null);
+	toHereMarker = null;
+	fromHereMarker = null;
 	contextmenuDir1.style.visibility="hidden";
 	contextmenuDir2.style.visibility="hidden";
 	contextmenuDir3.style.visibility="hidden";
