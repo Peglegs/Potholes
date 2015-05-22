61e173f8ac8bc72372b3a9bd748bcb8a08bcf8a5
Mark Norwich <mnorwich96@gmail.com>
2015-05-22 14:12:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("directions.html")
-
-if __name__ == "__main__":
-    app.debug = True
-    app.run(host="0.0.0.0", port=5000)
 import pymongo
 from pymongo import MongoClient 
 import json
@@ -19,6 +8,10 @@ app = Flask(__name__)
 client = MongoClient()
 db = client.potholes
 collection = db.potholes
+
+#@app.route("/")
+#def index():
+   # return render_template("directions.html")
 
 
 @app.route("/", methods=['Get','POST'])
